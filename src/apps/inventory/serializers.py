c8fdeeb9a3c143b1216541a9e96f44ad4e0db8ff
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2025-09-06 00:07:28 +0500

    inventory join

--- src/apps/inventory/serializers.py
+++ src/apps/inventory/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
 
+from apps.materials.serializers import MaterialSerializer
+from apps.organizations.serializers import OrganizationSerializer
+
 from .models import OrganizationInventory
 
 
-class InventorySerializer(serializers.ModelSerializer):
+class InventoryBaseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OrganizationInventory
         fields = "__all__"
+
+
+class CreateInventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationInventory
+        fields = ("organization", "material", "quantity")
+
+
+class GetInventorySerializer(serializers.ModelSerializer):
+    organization = OrganizationSerializer(read_only=True)
+    material = MaterialSerializer(read_only=True)
+
+    class Meta:
+        model = OrganizationInventory
+        fields = ("id", "organization", "material", "quantity", "created_at", "updated_at")
--- src/apps/inventory/views.py
+++ src/apps/inventory/views.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets
 
-from .serializers import InventorySerializer
+from .serializers import CreateInventorySerializer, GetInventorySerializer
 from .models import OrganizationInventory
 
 
 class InventoryViewset(viewsets.ModelViewSet):
     queryset = OrganizationInventory.objects.all()
-    serializer_class = InventorySerializer
+    serializer_class = CreateInventorySerializer
     filterset_fields = ["organization"]
+
+    def get_serializer_class(self):
+
+        if self.action in ["list", "retrieve"]:
+            return GetInventorySerializer
+
+        return self.serializer_class
